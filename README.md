8e5c5631b725ee793250f348d66bfd734f283067
dairycultist <kazumafan46@gmail.com>
2026-08-20 16:47:43 +0200

    Pivot to top-down style

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # Furry Roguelike Platformer
 
-- 2D platformer
-  - like mario since I like mario
-  - convenient since I use a laptop with no mouse (and also like controllers)
-- room-based roguelike
-  - like tboi/spelunky
+rimworld, strategy townbuilder, medieval buns, hmmm
+
+https://x.com/DimkaTheMonstr/status/2089379895851839758
+
+https://x.com/i/status/2085489595295150387
+
+https://e621.net/posts/4610461
+
+- playable on laptop with no mouse
+- roguelike elements
+  - maybe room-based like tboi/spelunky
   - also has the benefit of short rounds (= short respawn times) and no long-term progression (pick up for a 30m session and get the full experience), which is something I liked from CS:GO
+  - unique items that you collect throughout your run
 - customizable furry player character
   - like atlyss
 - multiplayer support
   - I like playing with friends
   - optional since I have no friends
-- unique guns and trinkets that you collect throughout your run
 - Godot
-- visual style is basically just NSMB: pixel perfect, pixel art terrain, 3D model entities
+- visual style is pixel-art entities on a 3D (downscaled) background
--- player.gd
+++ player.gd
@@ -1,46 +1,11 @@
-extends CharacterBody2D
-
-const CAMERA_DEADZONE_RADIUS = Vector2(24, 32);
-
-const MAX_SPEED = 200.0;
-const JUMP_VELOCITY = -350.0;
-const ACCELERATION = 350;
-const TURN_ACCELERATION = 1000;
-
-func _process(delta: float) -> void:
-	
-	# speen
-	$Mesh.rotation.y += delta;
-	
-	# camera follows player (replicated by moving 2DAnchor as there's no camera)
-	# with a deadzone of CAMERA_DEADZONE_RADIUS
-	@warning_ignore("integer_division")
-	var player_camera_off : Vector2 = Vector2(256 / 2, 192 / 2) - (get_parent().global_position + position);
-	
-	player_camera_off = player_camera_off.clamp(-CAMERA_DEADZONE_RADIUS, CAMERA_DEADZONE_RADIUS);
-	
-	@warning_ignore("integer_division")
-	get_parent().global_position = Vector2(256 / 2, 192 / 2) - (player_camera_off + position);
-	
-	# player mesh follows player
-	$Mesh.global_position = Vector3(global_position.x / 16, -global_position.y / 16, 0);
+extends CharacterBody3D
 
 func _physics_process(delta: float) -> void:
-	
-	# gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta;
 
-	# jumping
-	elif Input.is_action_just_pressed("player_jump"):
-		velocity.y = JUMP_VELOCITY;
+	global_rotation.y += Input.get_axis("turn_left", "turn_right") * delta;
 
-	# running
-	var direction := Input.get_axis("player_left", "player_right");
+	var direction := Input.get_vector("player_left", "player_right", "player_up", "player_down");
 	
-	if !direction or sign(direction) == sign(velocity.x):
-		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta);
-	else:
-		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, TURN_ACCELERATION * delta);
+	velocity = global_basis * Vector3(direction.x, 0, direction.y) * 5.0;
 
 	move_and_slide();
