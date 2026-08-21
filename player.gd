fb85c99606dfb3b08850eac8812ab71e5ee1d1d1
dairycultist <kazumafan46@gmail.com>
2026-08-21 11:27:39 +0200

    More robust camera controls; night light

--- player.gd
+++ player.gd
@@ -1,11 +1,30 @@
-extends CharacterBody3D
+extends Node3D
 
-func _physics_process(delta: float) -> void:
+@export var ZOOM_SPEED := 10.0;
+@export var PAN_SPEED_MULT := 0.8;
+@export var PAN_SPEED_SLOW_MULT := 0.4;
 
-	global_rotation.y += Input.get_axis("turn_left", "turn_right") * delta;
+var velocity : Vector3
 
-	var direction := Input.get_vector("player_left", "player_right", "player_up", "player_down");
+func _process(delta: float) -> void:
+
+	var dzoom := Input.get_axis("zoom_in", "zoom_out");
+
+	$Camera.size = clamp($Camera.size + dzoom * ZOOM_SPEED * delta, 6.0, 20.0);
+
+	if (!dzoom):
+		global_rotation.y += Input.get_axis("turn_right", "turn_left") * delta;
+
+	var direction_slow := Input.get_vector("pan_left_slow", "pan_right_slow", "pan_up_slow", "pan_down_slow");
+
+	if (direction_slow):
+		velocity = global_basis * Vector3(direction_slow.x, 0, direction_slow.y) * $Camera.size * PAN_SPEED_SLOW_MULT;
+	else:
+		var direction := Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down");
+		
+		if (direction):
+			velocity = global_basis * Vector3(direction.x, 0, direction.y) * $Camera.size * PAN_SPEED_MULT;
+		else:
+			velocity = lerp(velocity, Vector3.ZERO, 10.0 * delta);
 	
-	velocity = global_basis * Vector3(direction.x, 0, direction.y) * 5.0;
-
-	move_and_slide();
+	global_position += velocity * delta;
